47f7ea871b94d2fd618fb7642fba947cc1f62e77
Igor D <57639034+igor981@users.noreply.github.com>
2022-02-24 13:03:12 +0100

    new css, updated portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -10,25 +10,31 @@ const Portfolio = () => {
             <ul className='porto-scroll'>
             
                   <li className='card'>
-                      <h1>ProCo</h1>
-                      <p>
+                      <h1 className='port-name'>ProCo</h1>
+                      <p className='porto-description'>
                       A fullstack Javascript application for consulting firms and their employees. As a consultant you can register your availability and view your schedule, which is handled by the consulting firm. Consulting firms can use ProCo to assign consultants to companies, view their availability, and schedule them to work. They can also add companies and consultants to their roster. Authentication is required for use of ProCo, which is done through JSON web tokens.
 
                       </p>
                   </li>
                   <li className='card'>
-                      <h1>E-Commerce</h1>
-                      <p>
+                      <h1 className='port-name'>EZlog *In progress*</h1>
+                      <p className='porto-description'>
+                      A fullstack application using the PERN techstack. EZlog is a ticket system for bugs. As a user you can either create or join an organisation. The organisation consists of members, projects and tickets. You can open/close tickets and you can add individual users to tickets. 
+                      </p>
+                  </li>
+                  <li className='card'>
+                      <h1 className='port-name'>E-Commerce</h1>
+                      <p className='porto-description'>
                       A fullstack e-commerce webapplication built with the MERN techstack. React front-end sends GET requests to server side API and fetches products. On The client side I used Redux to dynamically display content on the page. The app comes a cart where you can add, update and remove products. 
                       </p>
                   </li>
                   <li className='card'>
-                      <h1>Atlas</h1>
-                      <p>Atlas is a SPA wikipedia-esque website that gathers data from several different APIs. The data inside API calls are used to call furthers APIs. This app is created with React Redux and uses a state management. The API calls are done through a backend built with express. </p>
+                      <h1 className='port-name'>Atlas</h1>
+                      <p className='porto-description'>Atlas is a SPA wikipedia-esque website that gathers data from several different APIs. The data inside API calls are used to call furthers APIs. This app is created with React Redux and uses a state management. The API calls are done through a backend built with express. </p>
                   </li>
                   <li className='card'>
-                      <h1>Todo App</h1>
-                      <p>
+                      <h1 className='port-name'>Todo App</h1>
+                      <p className='porto-description'>
                         The famous "Hello world" of coding projects. A CRUD todo app where you can create, read, update and delete todos from your todo list. Created with React-Redux
                       </p>
                   </li>
